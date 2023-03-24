9bcb93cf6cda3b42c8785ee9d6478ef2978dee80
Michal <khakaimic4@gmail.com>
2023-03-24 13:49:49 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const message = document.querySelector('p')
+console.log(message);
+
+document.addEventListener("DOMContentLoaded", function() {
+  message.innerHTML = "This is really cool!";
+});
+
